0737b67b7bf20ec3d3ead749bcd3dbf67088f458
sargon <niko@mail.velvet.moe>
2023-10-17 12:06:09 -0700

    fix: init tailwind

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+export default interface ButtonDef {
+    name: string;
+    goto: string;
+    data: object | undefined | null;
+}
---
+++ src/routes/modview/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type ButtonDef from "$lib";
+
+    let buttonDef: ButtonDef[] = [
+        {
+            name: "3v3",
+            goto: "/scenes/3v3",
+            data: null
+        },
+        {
+            name: "bracket",
+            goto: "/scenes/bracket",
+            data: null
+        },
+        {
+            name: "pool",
+            goto: "/scenes/pool",
+            data: null
+        },
+        {
+            name: "waiting",
+            goto: "/scenes/waiting",
+            data: null
+        }
+    ]
+</script>
+
+<div class="flex flex-row bg-stone-800 w-[100vw] h-[100vh] justify-center items-center">
+    <div class="flex flex-col w-screen h-screen justify-center items-center">
+        <div class="grid grid-flow-col col-auto gap-3">
+            {#each buttonDef as def}
+                <div class="bg-red-600 m-3 rounded-xl text-white text-2xl">
+                    {def.name}
+                </div>
+            {/each}
+        </div>
+    </div>
+</div>
